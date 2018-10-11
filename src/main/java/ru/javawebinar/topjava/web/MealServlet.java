e7669dc76d6ea00df229896cc0ac15438368e374
Ablaev.Sergey@gmail.com <serg256>
2018-10-11 09:48:30 +0500

    Merge branch 'HW01' of C:\Projects\topjava15\topjava with conflicts.

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,14 +1,12 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -19,19 +17,9 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("my redirect to meals");
 
-        Map parameterMap = request.getParameterMap();
-        if (!parameterMap.isEmpty()) {
-            switch ((String) parameterMap.get("action")) {
-                case "update":
-                    response.sendRedirect("update.jsp");
-                    break;
-                case "delete":
-                    response.sendRedirect("delete.jsp");
-            }
-        } else {
-            request.setAttribute("meals",MealsUtil.getUserMeal());
-        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-      //      response.sendRedirect("meals.jsp");
-        }
+      //  request.getParameterMap()
+
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("meals.jsp");
     }
 }
