6e2b96091665fc297be3405f33877384fa4946d4
sergey <Ablaev.Sergey@gmail.com>
2018-12-09 21:32:14 +0500

    HW07 repair errors

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -40,7 +40,7 @@ public class RootController {
     @GetMapping("/meals")
     public String meals(Model model) {
         model.addAttribute("meals",
-                MealsUtil.getWithExceeded(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
+                MealsUtil.getWithExcess(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         return "meals";
     }
 }
